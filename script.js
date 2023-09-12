4db32e8d3d4bbfde0bace3a263abec8d03ee730c
AffanShaikh100 <133389464+AffanShaikh100@users.noreply.github.com>
2023-09-12 18:41:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 //your code here
 
+let button = document.getElementById("swap");
+let main = document.getElementById("app");
+function toggle(){
+	button.classList.toggle("button_night");
+	main.classList.toggle("night");
+}
+
+button.addEventListener("click",toggle);
